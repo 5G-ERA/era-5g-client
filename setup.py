d52fae988a0602d07441b0fd13bffc092edac843
Petr Dobeš <idobes@fit.vutbr.cz>
2023-02-23 12:29:47 +0100

    feat: Add setup.py to allow editable installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='era_5g_client',
+      version='0.0.1',
+      description='A client for 5G-ERA NetworkApps',
+      packages=['era_5g_client']
+     )
